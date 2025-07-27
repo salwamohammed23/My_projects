3e4c960ce2189ee4e42340fb22a73ff31e0e449e
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2025-07-27 20:13:07 +0300

    Update face_recognition.py

--- Face Recognition/face_recognition.py
+++ Face Recognition/face_recognition.py
@@ -1,33 +1,3 @@
-# **AI Lab: Deep Learning for Computer Vision**
-# **WorldQuant University**
-#
-#
-
-# **Usage Guidelines**
-#
-# This file is licensed under Creative Commons Attribution-NonCommercial-
-# NoDerivatives 4.0 International.
-#
-# You **can** :
-#
-#   * ✓ Download this file
-#   * ✓ Post this file in public repositories
-#
-# You **must always** :
-#
-#   * ✓ Give credit to WorldQuant University for the creation of this file
-#   * ✓ Provide a link to the license
-#
-# You **cannot** :
-#
-#   * ✗ Create derivatives or adaptations of this file
-#   * ✗ Use this file for commercial purposes
-#
-# Failure to follow these guidelines is a violation of your terms of service and
-# could lead to your expulsion from WorldQuant University and the revocation
-# your certificate.
-#
-#
 
 # Import needed libraries
 import matplotlib
@@ -146,4 +116,4 @@ def add_labels_to_image(image):
     return fig
 
 
-# This file © 2024 by WorldQuant University is licensed under CC BY-NC-ND 4.0.
+
